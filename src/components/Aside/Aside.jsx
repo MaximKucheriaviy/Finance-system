8bc3e14040f08e66209d4676b561467ce481ef2a
Maxim Kucheriaviy <maxim-k@i.ua>
2022-10-12 10:41:15 +0300

    some works

--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -1,6 +1,31 @@
 import { AsideStyled } from "./AsideStyled"
+import { useEffect } from "react"
 
 export const Aside = () => {
+
+
+    useEffect(() => {
+        const currensyUpdate = () => {
+            console.log("currensyUpdate");
+        }
+        const localDate = {
+            updateDate: Date.now(),
+        }
+        let currencyInfo = window.localStorage.getItem("currencyInfo");
+        if(!currencyInfo){
+            window.localStorage.setItem("currencyInfo", JSON.stringify(localDate));
+            currensyUpdate();
+            return;
+        }
+        currencyInfo = JSON.parse(currencyInfo);
+        const storageDate = new Date(currencyInfo.updateDate);
+        const nowDate = new Date(localDate.updateDate);
+        if(storageDate.getDate() !== nowDate.getDate() || storageDate.getMonth() !== nowDate.getMonth() || storageDate.getFullYear() !== nowDate.getFullYear()){
+            window.localStorage.setItem("currencyInfo", JSON.stringify(localDate));
+            currensyUpdate();
+        }
+    }, []);
+
     return <AsideStyled>
         this is aside
     </AsideStyled>
