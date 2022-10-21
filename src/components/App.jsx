6885efd40ff31ca8f605f155764a81eac2ce5ec7
Maxim Kucheriaviy <maxim-k@i.ua>
2022-10-21 22:57:09 +0300

    some comit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,28 +7,51 @@ import { Main } from "pages/Main/Main";
 import { Statistic } from "pages/Statistic/Statistic";
 import { Calculator } from "pages/Calculator/Calculator";
 import { Usages } from "pages/Usages/Usages";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+
 
 const theme = {
   mainBackground: "wheat",
   borderColor: "black"
 }
 
+const initialState = {
+  userName: ""
+}
+
+const rootReduser = (state = initialState, action) => {
+  switch (action.type){
+    case "userName/setUsername":
+      return {
+        userName: action.payload
+      }
+    default:
+      return state;
+  }
+};
+
+const strore = configureStore({
+  reducer: rootReduser
+})
 
 export const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <AppStyled>
-        <Aside/>
-        <PageContainer>
-          <Header/>
-          <Routes>
-            <Route path="/" element={<Main/>}/>
-            <Route path="statistic" element={<Statistic/>}/>
-            <Route path="calculator" element={<Calculator/>}/>
-            <Route path="usages" element={<Usages/>}/>
-          </Routes>
-        </PageContainer>
-      </AppStyled>
-    </ThemeProvider>
+    <Provider store={strore}>
+      <ThemeProvider theme={theme}>
+        <AppStyled>
+          <Aside/>
+          <PageContainer>
+            <Header/>
+            <Routes>
+              <Route path="/" element={<Main/>}/>
+              <Route path="statistic" element={<Statistic/>}/>
+              <Route path="calculator" element={<Calculator/>}/>
+              <Route path="usages" element={<Usages/>}/>
+            </Routes>
+          </PageContainer>
+        </AppStyled>
+      </ThemeProvider>
+    </Provider>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
 import './index.css';
+import { createContext } from "react";
 
+const defaultContext = {
+  logedUser: "qwewqwewqe",
+  setLogedUser: (user) => {
+    this.logedUser = user;
+  }
+}
+
+export const UserContext = createContext()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/Finance-system'>
-        <App />
-    </BrowserRouter>
+    <UserContext.Provider value={defaultContext}>
+      <BrowserRouter basename='/Finance-system'>
+          <App />
+      </BrowserRouter>
+    </UserContext.Provider>
   </React.StrictMode>
 );
