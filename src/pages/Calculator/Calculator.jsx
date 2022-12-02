1d50268d419e9d9d9a124058e48e0694bc0889b0
DarlingHedgehog <maximvasilenko2004@gmail.com>
2022-12-02 20:11:55 +0200

    Опис Калькулятори

--- src/pages/Calculator/Calculator.jsx
+++ src/pages/Calculator/Calculator.jsx
@@ -10,7 +10,7 @@ export const Calculator = () => {
     return <CalculatorStyled>
         <Container>
             <h2>Калькулятор</h2>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum quas voluptas sequi quasi facere delectus architecto consequuntur nisi deserunt! Delectus dolorem, doloremque aliquid sequi ratione expedita natus. Corporis, cum! Dolorum dolore non odit quam natus atque minima assumenda tempora debitis iste nobis, error nostrum sequi facilis excepturi. Delectus illum excepturi exercitationem? Ea quae neque, maiores quaerat eligendi quibusdam ullam dolore, enim incidunt autem odit architecto beatae excepturi placeat! Eos voluptatibus vero repudiandae, sequi unde, provident esse, voluptates quos rerum dignissimos omnis impedit harum qui possimus id non dicta reprehenderit. Molestiae quia corporis officiis quos nisi saepe minus facere, iure maiores.</p>
+            <p>На цій сторінці ми можемо запропонувати вам інструменти для планування ваших депозитів, а також конвертер валют</p>
             <DateShow date={testDate.value}/>
             <button onClick={() => {dispatch(incrementMonth())}}>increment date</button>
             <button onClick={() => {dispatch(decrementMonth())}}>decrement date</button>
